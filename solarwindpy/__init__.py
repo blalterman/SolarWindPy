1cc6c7f0f1ac4525fe80c12fa2b7d66e69ed0b83
blalterman <12834389+blalterman@users.noreply.github.com>
2019-02-02 21:46:08 -0500

    Changed versioning tools

--- solarwindpy/__init__.py
+++ solarwindpy/__init__.py
@@ -1,30 +1,18 @@
 #!/usr/bin/env python
-r"""
-Name   : __init__
-Author : B. L. Alterman
-e-mail : balterma@umich.edu
-
-Description
------------
--Python tools for solar wind data analysis.
-
-
-Propodes Updates
-----------------
--
-
-Do Not Try
-----------
--
-
-Notes
------
--
-
-"""
-__version__ = "0.0.1.dev"
-
 import pdb
 import logging
+from pkg_resources import get_distribution, DistributionNotFound
 
 from . import units_constants, base, vector, tensor, ions, plasma
+
+__all__ = ["plasma", "ions", "tensor", "vector", "base", "units_constants"]
+
+__author__ = "B. L. Alterman <balterma@umich.edu>"
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
+    
+# __version__ = "0.0.1.dev"
