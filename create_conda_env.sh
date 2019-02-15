835bdf21e73c3eac0fb6d7941d5c8b6317b097bb
blalterman <alterman.ben@gmail.com>
2019-02-15 00:20:19 -0500

    W  Started solar activity directory

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-conda create -n $1 pandas scipy numpy numexpr bottleneck pycodestyle pydocstyle setuptools twine wheel matplotlib astropy pycodestyle flake8 black pre_commit
+conda create -n $1 pandas scipy numpy numexpr bottleneck pycodestyle pydocstyle setuptools twine wheel matplotlib astropy pycodestyle flake8 black pre_commit sphinx
---
+++ solarwindpy/solar_activity/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+Name   : solar_activity.py
+Author : B. L. Alterman
+e-mail : balterma@umich.edu
+alias  : sa
+
+Description
+-----------
+-Contains solar activity analysis download and analysis tools.
+
+Propodes Updates
+----------------
+-
+
+Notes
+-----
+-
+
+"""
+import pdb  # noqa: F401
+
+# import logging
+
+# import re as re
+# import numpy as np
+# import pandas as pd
