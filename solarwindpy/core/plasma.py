55d806f36d99d92d9307d7a64fe6f569078968f0
blalterman <alterman.ben@gmail.com>
2019-02-16 23:26:03 -0500

    P  cleaned all non-test sphinx errors

--- solarwindpy/core/plasma.py
+++ solarwindpy/core/plasma.py
@@ -10,18 +10,25 @@ Propodes Updates
 -It would be cute if one could call `plasma % a`, i.e. plasma mod
  an ion and return a new plasma without that ion in it. Well, either
  mod or subtract. Subtract and add probably make more sense. (20180129)
+
 -See (https://drive.google.com/drive/folders/0ByIrJAE4KMTtaGhRcXkxNHhmY2M)
  for the various methods that might be worth considering including __getattr__
  vs __getattribute__, __hash__, __deepcopy__, __copy__, etc. (20180129)
+
 -Convert `Plasma.__call__` to `Plasma.__getitem__` and `Plasma.__iter__` to
  to allow iterating over ions. (20180316)
  N.B. This could have complicated results as to how we actually access the
  underlying data and objects stored in the DataFrame.
+
 -Define `__format__` methods for use with `str.format`. (20180316)
+
 -Define `Plasma.__len__` to return the number of ions in the plasma. (20180316)
+
 -Split each class into its own file. Suggested by EM. (BLA 20180217)
+
 -Add `Plasma.dropna(*args, **kwargs)` that passes everything to `plasma.data.dropna`
  and then calls `self.__Plasma__set_ions()` to update the ions after drop. (20180404)
+
 -Moved `_conform_species` to base.Base so that it is accessable for
  alfvenic_turbulence.py. Did not move tests out of `test_plasma.py`.  (20181121)
 """
