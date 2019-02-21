f9656c2474091e883165ca3c4063836835b0f2ac
Benjamin L. Alterman <balterma@umich.edu>
2019-02-21 15:22:05 -0500

    P  Updating env create script

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-conda create -n $1 pandas scipy numpy numexpr bottleneck pycodestyle pydocstyle setuptools twine wheel matplotlib astropy pycodestyle flake8 black pre_commit sphinx
+conda create -n $1 python=3 pandas scipy numpy numexpr bottleneck pycodestyle pydocstyle setuptools twine wheel matplotlib astropy pycodestyle flake8 black pre_commit sphinx pre_commit
