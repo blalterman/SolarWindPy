f2318ad9bb7270e8de388008cb18ce3e18fddfd2
blalterman <alterman.ben@gmail.com>
2019-02-27 22:34:04 -0500

    P  Added to env create script

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -6,14 +6,22 @@ conda create -n $1 python=3 \
                    numpy \
                    numexpr \
                    bottleneck \
+                   matplotlib \
+                   jupyter \
+                   nbdime \
+                   widgetsnbextension \
+                   yaml \
+                   pyyaml \
+                   astropy \
                    setuptools \
                    twine \
                    wheel \
-                   matplotlib \
-                   astropy \
                    flake8 \
                    black \
                    pre_commit \
                    sphinx \
                    sphinx_rtd_theme \
                    pre_commit
+
+conda activate $1
+
