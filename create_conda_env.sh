a025c9348dd1a909faaee45e191cdd258b7cbe91
blalterman <alterman.ben@gmail.com>
2019-02-27 22:40:22 -0500

    P  editted env create script

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,27 +1,32 @@
 #! /bin/sh
 
-conda create -n $1 python=3 \
-                   pandas \
-                   scipy \
-                   numpy \
-                   numexpr \
-                   bottleneck \
-                   matplotlib \
-                   jupyter \
-                   nbdime \
-                   widgetsnbextension \
-                   yaml \
-                   pyyaml \
-                   astropy \
-                   setuptools \
-                   twine \
-                   wheel \
-                   flake8 \
-                   black \
-                   pre_commit \
-                   sphinx \
-                   sphinx_rtd_theme \
-                   pre_commit
+if [[ "$#" -eq 0 ]]; then
+    read -spt 60 "Environment name?" envname
+else
+    envname=$1
+
+conda create -n $envname python=3 \
+                         pandas \
+                         scipy \
+                         numpy \
+                         numexpr \
+                         bottleneck \
+                         matplotlib \
+                         jupyter \
+                         nbdime \
+                         widgetsnbextension \
+                         yaml \
+                         pyyaml \
+                         astropy \
+                         setuptools \
+                         twine \
+                         wheel \
+                         flake8 \
+                         black \
+                         pre_commit \
+                         sphinx \
+                         sphinx_rtd_theme \
+                         pre_commit
 
 conda activate $1
 
