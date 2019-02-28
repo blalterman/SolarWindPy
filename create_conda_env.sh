9235ebe9fa924a6f7cf6f7de3436d7b4d34f070f
blalterman <alterman.ben@gmail.com>
2019-02-27 22:29:15 -0500

    P  Updated conda create script

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,3 +1,19 @@
 #! /bin/sh
 
-conda create -n $1 python=3 pandas scipy numpy numexpr bottleneck pycodestyle pydocstyle setuptools twine wheel matplotlib astropy pycodestyle flake8 black pre_commit sphinx pre_commit
+conda create -n $1 python=3 \
+                   pandas \
+                   scipy \
+                   numpy \
+                   numexpr \
+                   bottleneck \
+                   setuptools \
+                   twine \
+                   wheel \
+                   matplotlib \
+                   astropy \
+                   flake8 \
+                   black \
+                   pre_commit \
+                   sphinx \
+                   sphinx_rtd_theme \
+                   pre_commit
