f89c9ce91a882a2fe60e342c6b9b8f6c70e0a6f4
blalterman <alterman.ben@gmail.com>
2019-11-21 16:22:31 -0500

    Bf  Trying to fix setup.cfg requriements

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -15,30 +15,7 @@ use="jupyter nbdime widgetsnbextension yaml pyyaml astropy sunpy"
 
 pkgs="$core $dev $use"
 
-conda create -n $envname python=3 $pkgs
-#                         pandas \
-#                         scipy \
-#                         numpy \
-#                         numexpr \
-#                         bottleneck \
-#                         matplotlib \
-#                         pytables \
-#                         #jupyter \
-#                         #nbdime \
-#                         #widgetsnbextension \
-#                         #"mkl_fft=1.0.6" \
-#                         yaml \
-#                         pyyaml \
-#                         #"pyyaml>=4.2b1" \
-#                         astropy \
-#                         #setuptools \
-#                         #twine \
-#                         #wheel \
-#                         #flake8 \
-#                         #black \
-#                         #sphinx \
-#                         #sphinx_rtd_theme \
-#                         #pre_commit
-#
+conda create -n $envname python=3.7 $pkgs
+
 #conda activate $envname
 
