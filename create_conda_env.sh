201dfe729c939820c3df4f9540140fc9d8e50d71
blalterman <blaltermanphd@gmail.com>
2020-02-24 21:40:49 -0600

    P  Attempting requirements bump

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -13,7 +13,8 @@ core="pandas scipy numpy numexpr bottleneck matplotlib pytables"
 dev="setuptools twine wheel flake8 black sphinx sphinx_rtd_theme pre_commit"
 use="jupyter nbdime widgetsnbextension yaml pyyaml astropy sunpy heliopy cdflib"
 
-pkgs="$core $dev $use"
+#pkgs="$core $dev $use"
+pkgs="$core $dev"
 
 conda create -n $envname python=3.7 $pkgs
 
